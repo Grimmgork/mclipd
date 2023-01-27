2a29023004bcb807a10b2ea3aaffab82c3bc5277
root <root@guest.guest>
2023-01-27 19:44:20 +0000

    fix send_response_chunked

--- mclipd.pl
+++ mclipd.pl
@@ -2,10 +2,14 @@ use strict;
 use HTTP::Daemon;
 use HTTP::Status;
 use HTTP::Status qw(:constants);
+use Bytes::Random::Secure qw( random_string_from );
 use feature 'state';
+use Time::Piece;
+use MIME::Types;
 
 use constant CHUNKSIZE	=> 255;
 use constant APIKEY		=> "secretkey";
+use constant MIMETYPES	=> MIME::Types->new;
 
 $SIG{PIPE} = 'IGNORE'; # prevent perl from quitting if trying to write to a closed socket ???
 
@@ -15,13 +19,16 @@ my $d = HTTP::Daemon->new(
 	Timeout => 1
 ) || die;
 
+print get_new_access_token(), "\n";
+print localtime->strftime('%Y%m%d-%H%M%S'), "\n";
+
 print "starting metaclip server ...\n";
 print "<URL:", $d->url, ">\n";
 
 my $clipboard_data;
 my $clipboard_type;
-my $clipboard_name = "test.txt";
-my $clipboard_share = "secretlocation";
+my $clipboard_name;
+my $clipboard_share = get_new_access_token();
 
 while(1) {
     my $c = $d->accept || next;
@@ -53,12 +60,14 @@ sub send_response_chunked{
 		$res->remove_header("transfer-encoding");
 	}
 	$c->send_status_line($res->code);
-	foreach my $key (keys %{$res->headers}){
-		$c->send_header(%{$res->headers}{$key});
+	my @fieldnames = $res->header_field_names;
+	foreach my $key (@fieldnames){
+		$c->send_header($key, $res->header($key));
 	}
-	return if $res->code == HTTP_NO_CONTENT;
 
 	print $c "\n";
+	return if $res->code == HTTP_NO_CONTENT;
+
 	my $i = 0;
 	while(my $chunk = substr $res->content, $i, CHUNKSIZE){
 		my $hex = sprintf("%X", length $chunk);
@@ -75,9 +84,9 @@ sub get_response{
 	my $authenticated = 1 if $req->header("apikey") eq APIKEY;
 
 	# GET /static/*
-	if($req->uri->path =~ /^\/static\// and $req->method eq 'GET'){
+	if($req->method eq 'GET' and $req->uri->path =~ /^\/static\//){
 		return status_message_res(404) unless my $data = read_static_file($req->uri->path);
-		my $mimetype = get_mime_type($req->uri->path);
+		my $mimetype = MIMETYPES->mimeTypeOf($req->uri->path);
 		my @header;
 		if($mimetype){
 			push @header, "x-content-type-options" => "nosniff";
@@ -86,21 +95,18 @@ sub get_response{
 		return HTTP::Response->new(200, undef, \@header, $data);
 	}
 
-	# GET /[secret]/[file.txt]
+	# GET /[secretlocation]/[file.txt]
 	if($req->method eq 'GET' and my ($share, $filename) = $req->uri->path =~ m/^\/([^\/ ]+)\/([^\/ ]+)/){
 		print "$share + $filename\n";
 		return status_message_res(403) unless ($share eq $clipboard_share and $filename eq $clipboard_name);
-		print "match!\n";
-		$clipboard_share = chomp($clipboard_share = `cat /proc/sys/kernel/random/uuid`);
+		print "anonymous access granted!\n";
+		$clipboard_share = get_new_access_token(); # move to a new safer location ~
 		unless($clipboard_data){
 			return status_message_res(204); # 204 empty response!
 		}
-		my @header;
-		if($clipboard_type){
-			push @header, "content-type" => $clipboard_type;
-			push @header, "x-content-type-options" => "nosniff";
-		}
-		return HTTP::Response->new(200, undef, \@header, $clipboard_data);
+		my $res = HTTP::Response->new(200, undef, undef, $clipboard_data);
+		$res->header("content-type" => $clipboard_type, "x-content-type-options" => "nosniff") if $clipboard_type;
+		return $res;
 	}
 
 	return status_message_res(403) unless $authenticated;
@@ -110,48 +116,46 @@ sub get_response{
 		print "redirect\n";
 		# GET /
 		if($req->method eq 'GET'){
+			return status_message_res(204) unless $clipboard_data; # 204 empty response! 
 			return HTTP::Response->new(307, undef, ["location" => "/$clipboard_share/$clipboard_name"], undef);
 		}
 		# DELETE /
 		if($req->method eq 'DELETE'){
 			$clipboard_data = undef;
 			$clipboard_type = undef;
-			print "clear!\n";
+			$clipboard_share = get_new_access_token(); # move to a new safer location ~
+			print "cleared clipboard!\n";
 			return status_message_res(200);
 		}
 	}
 
 	# POST /[file.txt]
 	if($req->method eq 'POST' and my($filename) = $req->uri =~ /^\/([^\/ ]+)$/){
-		print "$filename\n";
 		$clipboard_data = $req->content || undef;
-		$clipboard_type = $req->header("content-type") || undef;
-		$clipboard_type = undef unless $clipboard_data;
-		$clipboard_share = chomp($clipboard_share = `cat /proc/sys/kernel/random/uuid`);
+		if($clipboard_data){
+			$clipboard_type = MIMETYPES->type($req->header("content-type")) || undef;
+			if($filename eq "undef"){
+				my @exts = $clipboard_type->extensions;
+				$filename = localtime->strftime('%Y%m%d-%H%M%S') . "." . $exts[0];
+				print "$filename\n";
+			}
+			$clipboard_name = $filename;
+		}
+		$clipboard_share = get_new_access_token(); # move to a new safer location ~
 		return status_message_res(200);
 	}
 
 	return status_message_res(404);
 }
 
+sub get_new_access_token{
+	return random_string_from( 'abcdefghijklmnop123456789-', 10 );
+}
+
 sub status_message_res{
 	my $code = shift;
 	my $message = status_message($code);
 	return HTTP::Response->new($code, undef, ["content-type" => "text/plain"], "$code - $message");
-}
-
-sub get_mime_type{
-	my $filename = shift;
-	state %hash = (
-        "html"	=> "text/html",
-        "ico"	=> "image/vnd.microsoft.icon",
-        "jpeg"	=> "image/jpeg",
-        "png"	=> "image/png",
-	   "txt"	=> "text/plain"
-    );
-    my ($ext) = $filename =~ /\.([^.]+)$/;
-    return undef unless defined $ext;
-    return $hash{$ext};
 }
 
 sub read_static_file{
